1f501752fb4ef2b33abd94aecf5351c56c61e4cf
MunIL HONG <bear4564@naver.com>
2025-03-17 11:38:47 +0900

    로그인완료git add .

--- src/main/java/com/korit/dreampath_back/dto/request/ReqLoginDto.java
+++ src/main/java/com/korit/dreampath_back/dto/request/ReqLoginDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Data
 @Schema(description = "로그인 DTO")
-public class ReqLoginDto {git
+public class ReqLoginDto {
     @Schema(description = "사용자이름")
     private String username;
     @Schema(description = "비밀번호")
