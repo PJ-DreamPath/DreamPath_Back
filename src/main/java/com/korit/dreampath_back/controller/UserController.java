dee0729ae6c39daffe4617f9376a963a4006d92e
Jeong-MinE <Jeong-MinE>
2025-03-18 11:24:48 +0900

    프로필 이미지 수정

--- src/main/java/com/korit/dreampath_back/controller/UserController.java
+++ src/main/java/com/korit/dreampath_back/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.korit.dreampath_back.controller;
 
+import com.korit.dreampath_back.dto.request.User.ReqUserUpdateDto;
+import com.korit.dreampath_back.dto.response.User.RespUserDto;
 import com.korit.dreampath_back.security.principal.PrincipalUser;
 import com.korit.dreampath_back.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -11,48 +15,64 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @GetMapping("/user/me")
+    @Operation(summary = "사용자 정보 조회")
+    public ResponseEntity<RespUserDto> getLoginUser(@AuthenticationPrincipal PrincipalUser principalUser) {
 
-    @PostMapping("/user/profile/img")
+        if (principalUser.getUser().getProfileImg() == null) {
+            principalUser.getUser().setProfileImg("default.png");
+        }
+
+        RespUserDto response = userService.getUserInfo(principalUser.getUser().getUserId());
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    @PostMapping(value = "/user/profile/img", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "프로필 이미지 수정")
     public ResponseEntity<?> changeProfileImg(
             @AuthenticationPrincipal PrincipalUser principalUser,
-            @RequestPart MultipartFile file) {
+            @RequestPart("file") MultipartFile file) {
 
         userService.updateProfileImg(principalUser.getUser(), file);
+
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/user/profile/nickname")
+    @Operation(summary = "닉네임 수정")
     public ResponseEntity<?> changeNickname(
             @AuthenticationPrincipal PrincipalUser principalUser,
-            @RequestBody Map<String, String> requestBody
-            ) {
-        String nickname = requestBody.get("nickname");
-        userService.updateNickname(principalUser.getUser(), nickname);
+            @RequestBody ReqUserUpdateDto reqUserUpdateDto) {
+
+        userService.updateNickname(principalUser.getUser(), reqUserUpdateDto.getNickname());
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/user/profile/password")
+    @Operation(summary = "비밀번호 수정")
     public ResponseEntity<?> changePassword(
             @AuthenticationPrincipal PrincipalUser principalUser,
-            @RequestBody Map<String, String> requestBody
-    ) {
-        String password = requestBody.get("password");
-        userService.updatePassword(principalUser.getUser(), password);
+            @RequestBody ReqUserUpdateDto reqUserUpdateDto) {
+
+        userService.updatePassword(principalUser.getUser(), reqUserUpdateDto.getPassword());
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/user/profile/email")
+    @Operation(summary = "이메일 수정")
     public ResponseEntity<?> changeEmail(
             @AuthenticationPrincipal PrincipalUser principalUser,
-            @RequestBody Map<String, String> requestBody
-    ) {
-        String email = requestBody.get("email");
-        userService.updateEmail(principalUser.getUser(), email);
+            @RequestBody ReqUserUpdateDto reqUserUpdateDto) {
+
+        userService.updateEmail(principalUser.getUser(), reqUserUpdateDto.getEmail());
         return ResponseEntity.ok().build();
     }
 }
