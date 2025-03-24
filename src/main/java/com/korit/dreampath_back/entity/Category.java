b46eb9c6fad5b27897ef5b64d6a2300d23059d50
elin-sunhye <tjsgp43@gmail.com>
2025-03-24 11:10:56 +0900

    category 수정

--- src/main/java/com/korit/dreampath_back/entity/Category.java
+++ src/main/java/com/korit/dreampath_back/entity/Category.java
@@ -13,5 +13,6 @@ import lombok.NoArgsConstructor;
 public class Category {
     private int categoryId;
     private String categoryName;
+    private String categoryNameKor;
     private int boardId;
 }
